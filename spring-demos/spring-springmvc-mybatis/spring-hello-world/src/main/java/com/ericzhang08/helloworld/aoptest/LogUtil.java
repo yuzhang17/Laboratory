2426fcb82a9d18019035261b40c96314048c9a80
yuzhang17 <yuzhangfish@163.com>
2020-10-09 22:52:48 +0800

    add pointcut test

--- spring-demos/spring-springmvc-mybatis/spring-hello-world/src/main/java/com/ericzhang08/helloworld/aoptest/LogUtil.java
+++ spring-demos/spring-springmvc-mybatis/spring-hello-world/src/main/java/com/ericzhang08/helloworld/aoptest/LogUtil.java
@@ -10,24 +10,27 @@ import java.util.Arrays;
 
 @Component
 public class LogUtil {
-    @Before(value = "execution(public  int com.ericzhang08.helloworld.aoptest.Calculator.*(int, int))")
+    @Pointcut("execution(public  int com.ericzhang08.helloworld.aoptest.Calculator.*(int, int))")
+    public void pointcut(){}
+
+    @Before(value = "pointcut()")
     public static void logStart(JoinPoint joinPoint) {
         System.out.println("args:" + Arrays.toString(joinPoint.getArgs()));
         System.out.println("method  start");
     }
 
-    @AfterReturning(value = "execution(public  int com.ericzhang08.helloworld.aoptest.Calculator.*(int, int))", returning = "result")
+    @AfterReturning(value = "pointcut()", returning = "result")
     public static void logReturn(int result) {
         System.out.println("method return value:" + result);
         System.out.println("method return");
     }
 
-    @AfterThrowing(value = "execution(public  int com.ericzhang08.helloworld.aoptest.Calculator.*(int, int))", throwing = "exception", )
+    @AfterThrowing(value = "pointcut()", throwing = "exception")
     public static void logException(Exception exception) {
         System.out.println("throw exception, exception is...");
     }
 
-    @After("execution(public  int com.ericzhang08.helloworld.aoptest.Calculator.*(int, int))")
+    @After("pointcut()")
     public static void logEnd() {
         System.out.println("method end");
     }
