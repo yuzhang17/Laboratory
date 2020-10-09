b1ab330b6eafc0566084d27a23dfd019bff3f9ce
yuzhang17 <yuzhangfish@163.com>
2020-10-09 22:48:55 +0800

    add joinPoint test

--- spring-demos/spring-springmvc-mybatis/spring-hello-world/src/main/java/com/ericzhang08/helloworld/aoptest/LogUtil.java
+++ spring-demos/spring-springmvc-mybatis/spring-hello-world/src/main/java/com/ericzhang08/helloworld/aoptest/LogUtil.java
@@ -1,24 +1,29 @@
 package com.ericzhang08.helloworld.aoptest;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
 
 @Aspect
 
 @Component
 public class LogUtil {
-    @Before("execution(public  int com.ericzhang08.helloworld.aoptest.Calculator.*(int, int))")
-    public static void logStart() {
+    @Before(value = "execution(public  int com.ericzhang08.helloworld.aoptest.Calculator.*(int, int))")
+    public static void logStart(JoinPoint joinPoint) {
+        System.out.println("args:" + Arrays.toString(joinPoint.getArgs()));
         System.out.println("method  start");
     }
 
-    @AfterReturning("execution(public  int com.ericzhang08.helloworld.aoptest.Calculator.*(int, int))")
-    public static void logReturn() {
+    @AfterReturning(value = "execution(public  int com.ericzhang08.helloworld.aoptest.Calculator.*(int, int))", returning = "result")
+    public static void logReturn(int result) {
+        System.out.println("method return value:" + result);
         System.out.println("method return");
     }
 
-    @AfterThrowing("execution(public  int com.ericzhang08.helloworld.aoptest.Calculator.*(int, int))")
-    public static void logException() {
+    @AfterThrowing(value = "execution(public  int com.ericzhang08.helloworld.aoptest.Calculator.*(int, int))", throwing = "exception", )
+    public static void logException(Exception exception) {
         System.out.println("throw exception, exception is...");
     }
 
